6e94c738f8060194439513f1f182a3b31ac05ee0
WSMorrison <willimo@gmail.com>
2022-10-10 13:00:03 +0000

    Add function to run game and do addition operations.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,21 +12,32 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`Youclicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+runGame("addition");
+
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     //Create random integers between 1-24 (not 0-24, as it would with Math.floor but without +1)
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -45,7 +56,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
