a66943f0a4082aea8b5c0c2391eacdf252f1fdf6
WSMorrison <willimo@gmail.com>
2022-10-10 15:19:13 +0000

    Add code to keep track of correct and incorrect answers.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -51,8 +51,10 @@ function checkAnswer() {
     
     if (isCorrect) {
         alert ("Good job!");
+        incrementScore();
     } else {
         alert (`Too bad! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -78,11 +80,25 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM
+ * and increases it by one
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current wrong answer score from the Dom
+ * and increases it by one
+ */
 function incrementWrongAnswer() {
+
+    let oldIncorrect = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldIncorrect;
 
 }
 
