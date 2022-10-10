e121627cceb675b4a10190a5bdf8460edf1b2993
WSMorrison <willimo@gmail.com>
2022-10-10 12:20:14 +0000

    Add javascript function structure.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,37 @@
 console.log("Connected!");
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnser() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtratQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+    
+}
