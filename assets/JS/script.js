b63c8d901df932b5b35d0fb396154368fb150e62
WSMorrison <willimo@gmail.com>
2022-10-10 14:09:29 +0000

    Add function to calculate correct answer for quiz question.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -44,7 +44,22 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands and operator directly from
+ * the DOM, and returns the correct answer
+ */
 function calculateCorrectAnser() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
